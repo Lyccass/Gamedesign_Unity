08d2a444458b3493af1c0aff007ad54de9827120
Lammel17 <lukaskonstanz@web.de>
2023-04-28 14:38:10 +0200

    Keys and doors

---
+++ Psyché 2.0/Assets/Scripts/Door.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public int[] connectedLockIDs; // an array of the IDs of the locks connected to this door
+
+    private void Start()
+    {
+        // disable the door if any connected locks are still in the scene
+        foreach (int lockID in connectedLockIDs)
+        {
+            if (GameObject.Find("Lock" + lockID.ToString()) != null)
+            {
+                //gameObject.SetActive(false);
+                return;
+            }
+        }
+
+        // all connected locks have been removed, so enable the door
+        //gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        // check if all connected locks have been removed
+        foreach (int lockID in connectedLockIDs)
+        {
+            if (GameObject.Find("Lock" + lockID.ToString()) != null)
+            {
+                // this lock still exists, so the door should be disabled
+                //gameObject.SetActive(false);
+                return;
+            }
+        }
+
+        // all connected locks have been removed, so enable the door
+        //gameObject.SetActive(true);
+        Destroy(gameObject);
+    }
+
+}
---
+++ Psyché 2.0/Assets/Scripts/Lock.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock : MonoBehaviour
+{
+    public int lockID; // a unique ID for each lock
+    private bool canInteract = false;
+
+
+    private void Update()
+    {
+        if (canInteract && Input.GetKeyDown(KeyCode.E))
+        {
+            // check if the player has all three keys
+            if (Inventory.instance.HasKey(1) && Inventory.instance.HasKey(2) && Inventory.instance.HasKey(3))
+            {
+                // remove the lock from the scene
+                Debug.Log("Lock Opened");
+                Destroy(gameObject);
+
+                // check if all locks have been removed
+                bool allLocksRemoved = true;
+                foreach (Lock l in FindObjectsOfType<Lock>())
+                {
+                    if (l.gameObject.activeSelf)
+                    {
+                        allLocksRemoved = false;
+                        break;
+                    }
+                }
+
+                // open the door if all locks have been removed
+                if (allLocksRemoved)
+                {
+                    OpenDoor();
+                }
+            }
+            else
+            {
+                // remove the lock if the player has the specific key for it
+                if (Inventory.instance.HasKey(lockID))
+                {
+                    Debug.Log("Lock Opened");
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            canInteract = true;
+            Debug.Log("Maybe open Lock");
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            canInteract = false;
+            Debug.Log("open Lock no more");
+        }
+    }
+
+    private void OpenDoor()
+    {
+        // put your code here to open the door
+    }
+}
